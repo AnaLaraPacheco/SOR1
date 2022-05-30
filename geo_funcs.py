ea5b132951ebceeb37bb2ce57b6f67abe34ece7f
AnaLaraPacheco <100225863+AnaLaraPacheco@users.noreply.github.com>
2022-05-30 15:16:06 -0300

    Add files via upload

---
+++ geo_funcs.py
@@ -0,0 +1,12 @@
+def print_header(title):
+    print("Content-type:text/html\r\n\r\n")
+    print("""<html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>{}</title>
+                </head>
+                <body>""".format(title))
+
+
+def print_footer():
+    print("</body></html>")
